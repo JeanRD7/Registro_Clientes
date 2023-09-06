d9d5d6b86296014d8d84743409186f6eb6476b7c
JeanRD7 <jean64098@gmail.com>
2023-09-05 23:38:41 -0400

    Creacion del contexto de Ticket

---
+++ DAL/TicketContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Registro_Clientes.DAL;
+using Registro_Clientes.Models;
+
+namespace Registro_Clientes.DAL
+{
+    public class TicketContext : DbContext
+    {
+        public TicketContext(DbContextOptions<TicketContext> options) : base(options) { }
+
+        public DbSet<Tickets> Tickets { get; set; }
+    }
+}
