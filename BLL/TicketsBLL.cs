816243aec5454e0b2430a111611bf6f560d9749f
JeanRD7 <jean64098@gmail.com>
2023-09-06 00:17:09 -0400

    Creacion de  TicketsBLL

---
+++ BLL/TicketsBLL.cs
@@ -0,0 +1,14 @@
+﻿using Registro_Clientes.DAL;
+
+namespace Registro_Clientes.BLL
+{
+    public class TicketsBLL
+    {
+        private readonly TicketContext _contexto;
+
+        public TicketsBLL(TicketContext contexto)
+        {
+            _contexto = contexto;
+        }
+    }
+}
