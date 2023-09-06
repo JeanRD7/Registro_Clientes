631eba7115763856d5f7472033e586d36ee9ecee
JeanRD7 <jean64098@gmail.com>
2023-09-05 23:33:16 -0400

    Creacion del modelo de Ticket

---
+++ Models/Tickets.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Registro_Clientes.Models
+{
+    public class Tickets
+    {
+        [Key]
+
+        public int TicketId { get; set; }
+
+        [Required (ErrorMessage ="El nombre del Ticket es necesario")]
+
+        public string? Nombre { get; set;}
+    }
+}
